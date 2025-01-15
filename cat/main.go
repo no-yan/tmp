56658a0cdc869ed5247a93edfc36034779a11391
no-yan <63000297+no-yan@users.noreply.github.com>
2025-01-16 01:39:38 +0900

    chore: close each file after read

--- cat/main.go
+++ cat/main.go
@@ -18,36 +18,37 @@ func parse() []string {
 	return os.Args[1:]
 }
 
-func open(sources []string) (io.Reader, error) {
-	if len(sources) == 0 {
-		return os.Stdin, nil
-	}
-
-	rs := make([]io.Reader, len(sources))
-	for _, s := range sources {
-		f, err := os.Open(s)
-		if err != nil {
-			return nil, err
-		}
-		defer f.Close()
-		rs = append(rs, f)
-	}
-
-	return io.MultiReader(rs...), nil
-}
-
 func cat(srcs []string) error {
-	r, err := open(srcs)
-	if err != nil {
-		return err
+	// if src is empty, read from Stdin
+	if len(srcs) == 0 {
+		if _, err := io.Copy(os.Stdout, os.Stdin); err != nil {
+			return err
+		}
+		return nil
 	}
 
 	w := bufio.NewWriter(os.Stdout)
-	if _, err := io.Copy(w, r); err != nil {
-		return err
+	for _, src := range srcs {
+		if err := copyFileToWriter(src, w); err != nil {
+			return err
+		}
 	}
 
 	if err := w.Flush(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func copyFileToWriter(src string, w io.Writer) error {
+	f, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := io.Copy(w, f); err != nil {
 		return err
 	}
 
