fb7c2cffa7c466ec65228a52183a4798498bf38c
no-yan <63000297+no-yan@users.noreply.github.com>
2025-01-16 00:45:53 +0900

    feat: multi writer

--- cat/main.go
+++ cat/main.go
@@ -1,28 +1,30 @@
 package main
 
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
-	err := bigger()
+	f1, err := os.Open("sample1.txt")
 	if err != nil {
-		panic(err)
+		return
 	}
 
-	// f1, err := os.Open("sample1.txt")
-	// if err != nil {
-	// 	return
-	// }
-	//
-	// f2, err := os.Open("sample2.txt")
-	// if err != nil {
-	// 	return
-	// }
-	//
-	// r := io.MultiReader(f1, f2)
-	// w := bufio.NewWriter(os.Stdout)
-	// if _, err := io.Copy(w, r); err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// if err := w.Flush(); err != nil {
-	// 	return
-	// }
+	f2, err := os.Open("sample2.txt")
+	if err != nil {
+		return
+	}
+
+	r := io.MultiReader(f1, f2)
+	w := bufio.NewWriter(os.Stdout)
+	if _, err := io.Copy(w, r); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := w.Flush(); err != nil {
+		return
+	}
 }
