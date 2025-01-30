7ddb822349626272af49d163d3de114244c7f1d3
no-yan <63000297+no-yan@users.noreply.github.com>
2025-01-30 12:53:16 +0900

    feat: add errorServer

--- slowserver/main.go
+++ slowserver/main.go
@@ -17,7 +17,13 @@ func slowServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "End of data.")
 }
 
+func errorServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%s: %s\n", time.Now(), r.URL)
+	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+}
+
 func main() {
+	http.HandleFunc("/error", errorServer)
 	http.HandleFunc("/", slowServer)
 	fmt.Println("Starting slow server on :8080")
 	http.ListenAndServe(":8080", nil)
