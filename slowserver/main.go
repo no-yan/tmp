abf1ceb681a8b209527a2781ecc733d25bf6d7bf
no-yan <63000297+no-yan@users.noreply.github.com>
2025-02-02 17:31:21 +0900

    feat: fast server

--- slowserver/main.go
+++ slowserver/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"net/http"
 	"time"
 )
+
+func fastServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
 
 func slowServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("%s: %s\n", time.Now(), r.URL)
@@ -24,6 +29,7 @@ func errorServer(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/error", errorServer)
+	http.HandleFunc("/fast", fastServer)
 	http.HandleFunc("/", slowServer)
 	fmt.Println("Starting slow server on :8080")
 	http.ListenAndServe(":8080", nil)
