eaa468e1bda5057677c6d23ec1c278ebf6481b45
no-yan <63000297+no-yan@users.noreply.github.com>
2025-02-11 00:55:41 +0900

    refactor: create dir once

--- downloader/save.go
+++ downloader/save.go
@@ -7,20 +7,23 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
 type FileSaver struct {
-	dir string
+	dir  string
+	once *sync.Once
+	err  error
 }
 
 func NewFileSaver(dir string) *FileSaver {
-	return &FileSaver{dir: dir}
+	return &FileSaver{dir: dir, once: &sync.Once{}}
 }
 
 func (fs FileSaver) Save(r io.Reader, url string) (int64, error) {
-	err := os.Mkdir(fs.dir, 0o755)
-	if err != nil && !os.IsExist(err) {
-		panic(err)
+	err := fs.ensureDir()
+	if err != nil {
+		return 0, err
 	}
 
 	fName := fs.createFileName(url)
@@ -39,4 +42,16 @@ func (fs FileSaver) createFileName(url string) string {
 	b := sha256.Sum256([]byte(url))
 	hex := hex.EncodeToString(b[:])
 	return fmt.Sprintf("%s.log", hex)
+}
+
+func (fs *FileSaver) ensureDir() error {
+	fs.once.Do(func() {
+		err := os.MkdirAll(fs.dir, 0o755)
+		if err != nil && !os.IsExist(err) {
+			fs.err = err
+			return
+		}
+	})
+
+	return fs.err
 }
