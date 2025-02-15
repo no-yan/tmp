7b8cc9cb7b994f6fda1e1578053532be749d536b
no-yan <63000297+no-yan@users.noreply.github.com>
2025-02-15 09:19:05 +0900

    refactor

--- permutation/combination.go
+++ permutation/combination.go
@@ -35,7 +35,7 @@ func Combine(n, m int) ([][]int, error) {
 }
 
 func CombineLex(n, m int) [][]int {
-	result := make([][]int, 0, productRange(min(n-m+1, 0), n))
+	result := make([][]int, 0, binomial(n, m))
 	comb := make([]int, m)
 
 	var dfs func(int)
@@ -59,4 +59,33 @@ func CombineLex(n, m int) [][]int {
 	}
 	dfs(0)
 	return result
+}
+
+func binomial(n, k int) uint {
+	if k > n {
+		return 0
+	}
+
+	if k == 0 || k == n {
+		return 1
+	}
+
+	// 計算量を抑えるために nCk = nC(n-k) を利用
+	if k > n-k {
+		k = n - k
+	}
+
+	res := uint(1)
+	for i := 0; i < k; i++ {
+		// 累積的にかけて割る方法で大きな中間値を抑える
+		res *= uint(n-i) / uint(i+1)
+		// n-1/i+1 が割り切れる保証:
+		// 1. nCk は整数である
+		// 2. また、nC0 は1である
+		// 3. i回目のループでresはnCiに等しい
+
+		// 3(alt). nCk+1 = nCk * (n-1) / (i+1) の等式が成り立つ
+	}
+
+	return res
 }
